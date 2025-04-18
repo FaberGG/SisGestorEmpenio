c89f2658d607620a402ccf3b28c76d8d952c7d90
FaberGG <jhonfabernavia@gmail.com>
2025-04-18 00:14:09 -0500

    inserts para Prestamo y devolucion corregidos AHORA INSERTAN CORRECTAMENTE

--- SisGestorEmpenio/repository/DevolucionRepository.cs
+++ SisGestorEmpenio/repository/DevolucionRepository.cs
@@ -15,7 +15,7 @@ namespace SisGestorEmpenio.repository
         public bool guardar(Devolucion devolucion)
         {
             int filasAfectadas = 0;
-            string consulta = $"INSERT INTO Devolucion VALUES ({devolucion.GetPrestamo().GetCliente().GetId()}, {devolucion.GetPrestamo().GetArticulo().GetIdArticulo()}, DATE '{devolucion.GetFechaDevolucion().ToString("yyyy-MM-dd HH:mm:ss")}', {devolucion.GetMontoPagado().ToString(System.Globalization.CultureInfo.InvariantCulture)})";
+            string consulta = $"INSERT INTO Devolucion VALUES ({devolucion.GetPrestamo().GetCliente().GetId()}, {devolucion.GetPrestamo().GetArticulo().GetIdArticulo()}, NULL,  DATE '{devolucion.GetFechaDevolucion().ToString("yyyy-MM-dd")}', {devolucion.GetMontoPagado().ToString(System.Globalization.CultureInfo.InvariantCulture)})";
 
             filasAfectadas = dt.ejecutarDML(consulta);
             return filasAfectadas > 0;
--- SisGestorEmpenio/repository/PrestamoRepository.cs
+++ SisGestorEmpenio/repository/PrestamoRepository.cs
@@ -13,7 +13,7 @@ namespace SisGestorEmpenio.repository
         public bool guardar(Prestamo prestamo)
         {
             int filasAfectadas = 0;
-            string consulta = $"INSERT INTO Prestamo VALUES ({prestamo.GetCliente().GetId()}, {prestamo.GetArticulo().GetIdArticulo()}, '{prestamo.GetEstado().ToLower()}', DATE '{prestamo.GetFechaInicio().ToString("yyyy-MM-dd HH:mm:ss")}', DATE '{prestamo.CalcularFechaVencimiento().ToString("yyyy-MM-dd HH:mm:ss")}', {prestamo.GetTasaInteres().ToString(System.Globalization.CultureInfo.InvariantCulture)} , {prestamo.CalcularMontoTotal().ToString(System.Globalization.CultureInfo.InvariantCulture)})";
+            string consulta = $"INSERT INTO Prestamo VALUES ({prestamo.GetCliente().GetId()}, {prestamo.GetArticulo().GetIdArticulo()}, '{prestamo.GetEstado().ToLower()}', DATE '{prestamo.GetFechaInicio().ToString("yyyy-MM-dd")}', DATE '{prestamo.CalcularFechaVencimiento().ToString("yyyy-MM-dd")}', {prestamo.GetTasaInteres().ToString(System.Globalization.CultureInfo.InvariantCulture)} , {prestamo.CalcularMontoTotal().ToString(System.Globalization.CultureInfo.InvariantCulture)})";
 
             //VER CÓMO EN LA CONSULTA SE PONE LOS DATOS TIPO TIME 
 
